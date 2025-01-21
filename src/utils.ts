1b72db271535c20b1637981bb52c0b8f06c3d945
Torathion <torathion@gmail.com>
2025-01-21 21:51:14 +0100

    perf: determine platform only once

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
 import picomatch from 'picomatch';
 
+const isWin = process.platform === 'win32'
+
 // #region convertPathToPattern
 const ESCAPED_WIN32_BACKSLASHES = /\\(?![()[\]{}!+@])/g;
 export function convertPosixPathToPattern(path: string): string {
@@ -11,7 +13,7 @@ export function convertWin32PathToPattern(path: string): string {
 }
 
 export const convertPathToPattern: (path: string) => string =
-  process.platform === 'win32' ? convertWin32PathToPattern : convertPosixPathToPattern;
+  isWin ? convertWin32PathToPattern : convertPosixPathToPattern;
 // #endregion
 
 // #region escapePath
@@ -28,7 +30,7 @@ const WIN32_UNESCAPED_GLOB_SYMBOLS = /(?<!\\)([()[\]{}]|^!|[!+@](?=\())/g;
 export const escapePosixPath = (path: string): string => path.replace(POSIX_UNESCAPED_GLOB_SYMBOLS, '\\$&');
 export const escapeWin32Path = (path: string): string => path.replace(WIN32_UNESCAPED_GLOB_SYMBOLS, '\\$&');
 
-export const escapePath: (path: string) => string = process.platform === 'win32' ? escapeWin32Path : escapePosixPath;
+export const escapePath: (path: string) => string = isWin ? escapeWin32Path : escapePosixPath;
 // #endregion
 
 // #region isDynamicPattern
