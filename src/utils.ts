ee67f8c20f6223d4386b885877c7e14ba01f724a
Torathion <torathion@gmail.com>
2025-02-16 22:50:12 +0100

    change: overhaul fdir options building

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,5 @@
 import picomatch, { type Matcher } from 'picomatch';
-
-// #region PARTIAL MATCHER
-export interface PartialMatcherOptions {
-  dot?: boolean;
-  nocase?: boolean;
-}
+import type { PartialMatcherOptions } from './types';
 
 const ONLY_PARENT_DIRECTORIES = /^(\/?\.\.)+$/;
 
