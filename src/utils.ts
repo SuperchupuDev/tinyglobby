c4f5f117eb001867876a30d34b648ed0eb43351a
Torathion <torathion@gmail.com>
2025-02-20 14:51:03 +0100

    perf: replace startsWith with direct comparison for less overhead

--- src/utils.ts
+++ src/utils.ts
@@ -24,7 +24,7 @@ export function getPartialMatcher(patterns: string[], options?: PartialMatcherOp
     // normally the parent directory would not get crawled
     // and as such wrong results would be returned
     // to avoid this always return true if the input only consists of .. ../.. etc
-    if (input.startsWith('..') && ONLY_PARENT_DIRECTORIES.test(input)) {
+    if (input[0] === '.' && input[1] === '.' && ONLY_PARENT_DIRECTORIES.test(input)) {
       return true;
     }
     // no need to `splitPattern` as this is indeed not a pattern
