a7ff97631fd4158722257496f9ce7c4c89b4acc6
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2025-06-26 13:42:14 +0200

    add benchmarks (#122)
    * early attempt at benchmarks
    
    * rename bench workflow
    
    * do not use generated code
    
    makes typescript happy
    
    * add `packages/*/tsconfig.json` benchmark
    
    * drop `.mts` prefix from scripts
    
    * make biome happy

---
+++ benchmark/bench.ts
@@ -0,0 +1,51 @@
+import { glob as native } from 'node:fs/promises';
+import { join } from 'node:path';
+import fastGlob from 'fast-glob';
+import { glob } from 'glob';
+import { Bench } from 'tinybench';
+import { glob as tinyglobby } from '../src/index.ts';
+
+const bench = new Bench({ name: 'packages/*/tsconfig.json (typescript-eslint)' });
+
+bench
+  .add('tinyglobby', async () => {
+    await tinyglobby('packages/*/tsconfig.json', {
+      expandDirectories: false,
+      cwd: join(import.meta.dirname, 'typescript-eslint')
+    });
+  })
+  .add('fast-glob', async () => {
+    await fastGlob('packages/*/tsconfig.json', { cwd: join(import.meta.dirname, 'typescript-eslint') });
+  })
+  .add('glob', async () => {
+    await glob('packages/*/tsconfig.json', { cwd: join(import.meta.dirname, 'typescript-eslint') });
+  })
+  .add('node:fs glob', async () => {
+    await Array.fromAsync(native('packages/*/tsconfig.json', { cwd: join(import.meta.dirname, 'typescript-eslint') }));
+  });
+
+await bench.run();
+
+console.log(bench.name);
+console.table(bench.table());
+
+const bench2 = new Bench({ name: '**/* (typescript-eslint)' });
+
+bench2
+  .add('tinyglobby', async () => {
+    await tinyglobby('**/*', { expandDirectories: false, cwd: join(import.meta.dirname, 'typescript-eslint') });
+  })
+  .add('fast-glob', async () => {
+    await fastGlob('**/*', { cwd: join(import.meta.dirname, 'typescript-eslint') });
+  })
+  .add('glob', async () => {
+    await glob('**/*', { cwd: join(import.meta.dirname, 'typescript-eslint') });
+  })
+  .add('node:fs glob', async () => {
+    await Array.fromAsync(native('**/*', { cwd: join(import.meta.dirname, 'typescript-eslint') }));
+  });
+
+await bench2.run();
+
+console.log(bench2.name);
+console.table(bench2.table());
