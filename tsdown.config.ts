58ba17682f076cdc61e09b8748968edb9d665e1b
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2025-07-05 23:04:25 +0200

    cleanup configs

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig, type UserConfig } from 'tsdown/config';
 
 export default defineConfig({
-  dts: true,
-  entry: ['src/index.ts'],
   fixedExtension: true,
   format: ['esm', 'cjs'],
   nodeProtocol: 'strip',
-  sourcemap: !process.env.IS_RELEASE,
-  target: 'node12'
+  sourcemap: !process.env.IS_RELEASE
 }) as UserConfig;
