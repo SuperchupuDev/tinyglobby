d0d848c93bc0886da9f730a8e6044172c22e02f2
Madeline Gurriarán <53496941+SuperchupuDev@users.noreply.github.com>
2026-05-10 20:45:43 +0200

    reuse cwd in benchmark

--- benchmark/bench.ts
+++ benchmark/bench.ts
@@ -6,22 +6,20 @@ import { Bench } from 'tinybench';
 import { glob as tinyglobby } from '../src/index.ts';
 
 const bench = new Bench({ name: 'packages/*/tsconfig.json (typescript-eslint)' });
+const cwd = join(import.meta.dirname, 'typescript-eslint');
 
 bench
   .add('tinyglobby', async () => {
-    await tinyglobby('packages/*/tsconfig.json', {
-      expandDirectories: false,
-      cwd: join(import.meta.dirname, 'typescript-eslint')
-    });
+    await tinyglobby('packages/*/tsconfig.json', { expandDirectories: false, cwd });
   })
   .add('fast-glob', async () => {
-    await fastGlob('packages/*/tsconfig.json', { cwd: join(import.meta.dirname, 'typescript-eslint') });
+    await fastGlob('packages/*/tsconfig.json', { cwd });
   })
   .add('glob', async () => {
-    await glob('packages/*/tsconfig.json', { cwd: join(import.meta.dirname, 'typescript-eslint') });
+    await glob('packages/*/tsconfig.json', { cwd });
   })
   .add('node:fs glob', async () => {
-    await Array.fromAsync(native('packages/*/tsconfig.json', { cwd: join(import.meta.dirname, 'typescript-eslint') }));
+    await Array.fromAsync(native('packages/*/tsconfig.json', { cwd }));
   });
 
 await bench.run();
@@ -33,16 +31,16 @@ const bench2 = new Bench({ name: '**/* (typescript-eslint)' });
 
 bench2
   .add('tinyglobby', async () => {
-    await tinyglobby('**/*', { expandDirectories: false, cwd: join(import.meta.dirname, 'typescript-eslint') });
+    await tinyglobby('**/*', { expandDirectories: false, cwd });
   })
   .add('fast-glob', async () => {
-    await fastGlob('**/*', { cwd: join(import.meta.dirname, 'typescript-eslint') });
+    await fastGlob('**/*', { cwd });
   })
   .add('glob', async () => {
-    await glob('**/*', { cwd: join(import.meta.dirname, 'typescript-eslint') });
+    await glob('**/*', { cwd });
   })
   .add('node:fs glob', async () => {
-    await Array.fromAsync(native('**/*', { cwd: join(import.meta.dirname, 'typescript-eslint') }));
+    await Array.fromAsync(native('**/*', { cwd }));
   });
 
 await bench2.run();
